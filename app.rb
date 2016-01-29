7e738e2eb863358246650d8e940fb2b52fb5212a
andreymyster <and001@gmail.com>
2016-01-29 12:53:33 +0300

    Comments

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ def init_db
 end
 
 # формирование сообщения об ошибке
+# @error - переменная с текстом об ошибке, выводится в соответствующем вью
 # получает хеш и проверяет если в нем есть пустой ключ то добавляется соотв значение
 
 def set_error hh
@@ -54,10 +55,8 @@ end
 # обработка корневого запроса
 
 get '/' do
-
-	# сохранение в переменную БД Посты для вывода
+	# сохранение в переменную текст базы данных Посты для вывода
 	@result = @db.execute 'select * from posts order by id desc'
-
 	erb :index
 end
 
@@ -82,29 +81,45 @@ get '/details/:post_id' do
 	erb :details
 end
 
-# обработчик пост-запроса details
+# обработчик пост-запроса details - юзер отправляет комментарий к посту
 
 post '/details/:post_id' do
+
+	# получаем переменную номер поста из урл-а
 	post_id = params[:post_id]
+
+	# получаем содержание комментария
 	content = params[:content]
+
+	# добавляем в БД Комментариев эти переменные и дату написания
 	@db.execute 'insert into comments (content, created_date, post_id)
 		values (?,datetime(),?);', [content, post_id]
+
+	# редирект на эту же страницу с новым комментарием
 	redirect to ("/details/" + post_id)
 end
 
+# обработчик формы создания нового поста
+
 post '/new' do
+
+	# сохранение значений из формы в переменные - контент и автор
 	@content = params[:content]
 	@autor = params[:autor]
 
+	# создание хэша для вызова функции по формированию отчета об ошибках
 	hh = {:content => 'Введи текст поста', :autor => 'Представься'}
 	set_error hh
 
+	# если в переменной еррор что-то есть то выводим эту же страницу с сообщ об ошибках
 	if @error != ''
 		return erb :new
 	end
 
+	# добавляем в бд информацию о содержании поста, дате и авторе
 	@db.execute 'insert into posts (content, created_date, autor)
 		values (?,datetime(), ?);', [@content, @autor]
 
+	# редирект на корневую страницу с новым постом
 	redirect to '/'
 end
