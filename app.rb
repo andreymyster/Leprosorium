7d57708ba422638f8f6f2874c4a8d90a820146ee
andreymyster <and001@gmail.com>
2016-02-04 22:41:26 +0300

    Add comments but error at saving to DB Comment

--- app.rb
+++ app.rb
@@ -11,14 +11,14 @@ set :database, "sqlite3:lepra_new.db"
 # инициализация сущностей БД
 
 class Post < ActiveRecord::Base
-	has_many :comments, :foreign_key => "articleID"
+	has_many :comments, :foreign_key => "postID"
 	validates :content, presence: true
 	validates :author, presence: true
 end
 
 class Comment < ActiveRecord::Base
-	belongs_to :post, :foreign_key => "articleID"
-	validates :colcontent_com, presence: true
+	belongs_to :post, :foreign_key => "postID"
+	validates :content_com, presence: true
 end
 
 # обработка корневого запроса
@@ -37,15 +37,15 @@ end
 #вывод информации о посте
 get '/details/:post_id' do
 
-	# #получаем переменную из урла
-	# post_id = params[:post_id]
-	#
-	# #получаем список постов - у нас будет только один пост
-	# result = @db.execute 'select * from posts where id=?', [post_id]
-	# @row = result[0]
-	#
-	# #выбираем комментарии для нашего поста
-	# @comments = @db.execute 'select * from comments where post_id=?', [post_id]
+	#получаем переменную из урла
+	post_id = params[:post_id]
+
+	#получаем список постов - у нас будет только один пост по этому номеру
+	@result = Post.find post_id
+
+	#выбираем комментарии для нашего поста
+	# @comments = Comment.find_by post_id: post_id
+	@comments = Comment.all
 
 	erb :details
 end
@@ -54,26 +54,27 @@ end
 
 post '/details/:post_id' do
 
-	# получаем переменную номер поста из урл-а
-	post_id = params[:post_id]
+	# получаем содержание комментария и записываем в переменную бд
+	c = Comment.new params[:comment]
 
-	# получаем содержание комментария
-	content = params[:content]
+	# добавляем в переменную номер поста из параметров урл-а
+	c.post_id = params[:post_id]
 
-	# добавляем в БД Комментариев эти переменные и дату написания
-	# @db.execute 'insert into comments (content, created_date, post_id)
-		# values (?,datetime(),?);', [content, post_id]
+	# сохраняем переменную в бд
+	c.save!
+
 
 	# редирект на эту же страницу с новым комментарием
-	redirect to ("/details/" + post_id)
+	# redirect to ("/details/" + post_id)
 end
 
 # обработчик формы создания нового поста
 
 post '/new' do
 	# сохранение значений из формы в переменную
-	@p = Post.new params[:post]
-	@p.save
+	p = Post.new params[:post]
+	p.save
+
 	# редирект на корневую страницу с новым постом
 	redirect to '/'
 end
